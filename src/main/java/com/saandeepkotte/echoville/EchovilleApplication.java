b850b51a56cee63e9946190f26b1ee92238577f8
saandeepkotte <saandeepkotte5@gmail.com>
2025-07-13 14:41:35 +0530

    project init

---
+++ src/main/java/com/saandeepkotte/echoville/EchovilleApplication.java
@@ -0,0 +1,13 @@
+package com.saandeepkotte.echoville;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchovilleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EchovilleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saandeepkotte/echoville/EchovilleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saandeepkotte.echoville;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchovilleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
