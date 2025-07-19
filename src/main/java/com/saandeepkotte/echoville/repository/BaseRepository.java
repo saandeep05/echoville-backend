68d19db655e1df4abaf3d924ef7b0ac35b668585
saandeepkotte <saandeepkotte5@gmail.com>
2025-07-19 20:38:15 +0530

    Base repository (common methods): added commmon methods for common queries

--- src/main/java/com/saandeepkotte/echoville/repository/BaseRepository.java
+++ src/main/java/com/saandeepkotte/echoville/repository/BaseRepository.java
@@ -2,9 +2,20 @@ package com.saandeepkotte.echoville.repository;
 
 import com.saandeepkotte.echoville.model.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 @NoRepositoryBean
 public interface BaseRepository<T extends BaseEntity, U> extends JpaRepository<T, U> {
+    @Override
+    @Query("select t from #{#entityName} t where t.deletedAt is null")
+    List<T> findAll();
 
+    @Override
+    @Query("select t from #{#entityName} t where t.deletedAt is null and t.id = :id")
+    Optional<T> findById(@Param("id") U id);
 }
